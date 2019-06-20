b8b20f2654e2f2cd5c53ecad9f183c3c51e84545
kompanymwega <mwegagideon@gmail.com>
2019-06-20 20:09:46 +0300

    completed index route and major app styling

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(150), unique=True, nullable=False)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return '<Item %r>' % self.id
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('base.html')
+def Index():
+    if request.method == 'POST':
+        itemcontent = request.form['todo']
+        newitem = Item(content=itemcontent)
+
+        try:
+            db.session.add(newitem)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error creating new item'
+    else:
+        items = Item.query.order_by(Item.date_created).all()
+        return render_template('index.html', items=items)
 
 
 if __name__ == "__main__":
