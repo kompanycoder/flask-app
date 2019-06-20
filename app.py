374fc85b5582056b513c354ccaa11b73c555c7c9
kompanymwega <mwegagideon@gmail.com>
2019-06-20 05:16:32 +0300

    Added dependancies and run app with no errors using render template

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-class Item(content=''):
-    pass
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Item %r>' % self.id
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h3>Hello from Flask app</h3>'
+    return render_template('base.html')
 
 
 if __name__ == "__main__":
