a31cce2f3f309820065329ccc8aa71734bb1c0e7
kompanymwega <mwegagideon@gmail.com>
2019-07-25 18:52:41 +0300

    trying new design concepts

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+# init db session for the whole app
+db = SQLAlchemy(app)
+# do something with app...
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
