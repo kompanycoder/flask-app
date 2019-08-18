c38ced7fdd99ec9646145cc52b27ec1c6046690e
Gideon Mwega <mwegagideon@gmail.com>
2019-08-18 03:04:04 +0300

    to implement bootstrap files into the project for full offline support

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+Bootstrap(app)
+
 
 # init db session for the whole app
 db = SQLAlchemy(app)
