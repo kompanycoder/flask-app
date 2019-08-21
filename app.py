23b705767c53f70b856651de770f1d099289c006
Gideon Mwega <mwegagideon@gmail.com>
2019-08-21 15:48:16 +0300

    updated model class for items

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ db = SQLAlchemy(app)
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(150), unique=True, nullable=False)
-    date_created = db.Column(
-        db.DateTime, default=datetime.now(), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now, nullable=False)
     def __repr__(self):
         return '<Item %r>' % self.id
 
